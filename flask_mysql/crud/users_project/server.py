b9e6c271e2eca7a9859bc3f3bb3f1d45b86c81a3
Joe Park <joepark92@gmail.com>
2021-05-12 22:16:54 -0700

    updated for CRUD assignment

--- flask_mysql/crud/users_project/server.py
+++ flask_mysql/crud/users_project/server.py
@@ -22,11 +22,24 @@ def index():
     query = "SELECT * FROM users;"
 
     users = mysql.query_db(query)
-    print(users)
-
 
     return render_template("index.html", all_users = users)
 
+
+# READ One
+@app.route("/users/<int:user_id>")
+def display_user(user_id):
+    mysql = connectToMySQL("users_schema")
+    query = "SELECT * FROM users WHERE id = %(id)s;"
+    data = {
+        "id": user_id
+    }
+
+    user_list = mysql.query_db(query, data)
+
+    print(user_list[0])
+
+    return render_template("user.html", user = user_list[0])
 
 # CREATE
 @app.route("/users/new")
@@ -51,10 +64,49 @@ def create():
     return redirect("/")
 
 
-
 # UPDATE
+@app.route("/users/<int:user_id>/edit")
+def edit_user_form(user_id):
+    mysql = connectToMySQL("users_schema")
+    query = "SELECT * FROM users WHERE id = %(id)s;"
+    data = {
+        "id": user_id
+    }
+
+    user_list = mysql.query_db(query, data)
+
+    return render_template("edit.html", user = user_list[0])
+
+
+@app.route("/users/<int:user_id>/update", methods = ["POST"])
+def update_user(user_id):
+    mysql = connectToMySQL("users_schema")
+    query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, " \
+        "email = %(email)s, updated_at = NOW() WHERE id = %(id)s;"
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "id": user_id
+    }
+
+    mysql.query_db(query, data)
+
+    return redirect("/")
+
 
 # DELETE
+@app.route("/users/<int:user_id>/delete")
+def delete_user(user_id):
+    mysql = connectToMySQL("users_schema")
+    query = "DELETE FROM users WHERE id = %(id)s"
+    data = {
+        "id": user_id
+    }
+
+    mysql.query_db(query, data)
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
